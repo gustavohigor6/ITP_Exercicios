879887b85edcb232790dc9f2a02820f42a3920bc
gustavohigor <gustavohigor6@gmail.com>
2024-10-11 03:33:39 +0000

    inicio dos exercicios em estruturas de repetição

---
+++ unidade1/lacos/atv1lista1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Soma dos dígitos
+
+/* Escreva um programa que lê um número inteiro n e imprime a soma dos dígitos desse
+número. */
+
+int main () {
+    int num, soma, aux;
+
+    soma = 0;
+
+    cin >> num;
+
+    //Escrever como laço condicional.
+
+    do
+    {
+        aux = (num % 10);
+        soma = (soma + aux);
+        num = (num / 10);
+    } while (num != 0);
+    
+
+    /*while (num != 0) {
+        aux = (num % 10);
+        soma = (soma + aux);
+        num = (num / 10);
+    }*/
+
+    cout << soma << endl;
+
+    return 0;
+}
