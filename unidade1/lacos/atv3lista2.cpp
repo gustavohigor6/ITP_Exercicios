62c155cfd35674afacc6e5b16fa72d8d57900881
Gustavo Câmara <77999052+gustavohigor6@users.noreply.github.com>
2024-10-18 12:13:06 -0300

    Create atv3lista2.cpp

---
+++ unidade1/lacos/atv3lista2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+//Soma dos dígitos
+/* Escreva um programa que lê um número inteiro n e imprime a soma
+dos dígitos desse número. */
+
+int main () {
+  //Desenvolver a questão.
+  
+  return 0;
+}
